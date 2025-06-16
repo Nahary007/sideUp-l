0bc3ef92531e4f9d3837c2e240c5a833a1b99221
Nahary@007 <161349115+Nahary007@users.noreply.github.com>
2025-06-16 19:58:42 +0300

    Correction de la configuration CORS

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -1,6 +1,6 @@
 <?php
 return [
-    'paths' => ['api/*', '/login', '/logout', '/register', '/reserver', '/sanctum/csrf-cookie', '/contact'],
+    'paths' => ['api/*', '/login', '/logout', '/register', '/reserver', '/sanctum/csrf-cookie', '/contact', '/admin/check-auth'],
     'allowed_methods' => ['*'],
     'allowed_origins' => ['http://localhost:5173'],
     'allowed_origins_patterns' => [],
